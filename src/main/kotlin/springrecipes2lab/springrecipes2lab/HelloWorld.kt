7611ed69a2e00acd5ad27b5e80e74e96aec6c6fa
Victor Augusto <victoraweb@gmail.com>
2019-01-12 16:52:33 -0200

    [FEAT] Use propertties per profile

--- src/main/kotlin/springrecipes2lab/springrecipes2lab/HelloWorld.kt
+++ src/main/kotlin/springrecipes2lab/springrecipes2lab/HelloWorld.kt
@@ -1,10 +1,16 @@
 package springrecipes2lab.springrecipes2lab
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
 
 @Component
 class HelloWorld {
+    @Value("\${app.author}")
+    private lateinit var appAuthor: String
+
     @PostConstruct
     fun sayHello() = println("Hello from spring boot 2!")
+    @PostConstruct
+    fun printAuthor() = println(appAuthor)
 }
