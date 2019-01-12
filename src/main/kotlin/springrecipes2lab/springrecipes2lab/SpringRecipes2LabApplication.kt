0da312e9c195daac2f5c8dbfdb11590982ce73d9
Victor Augusto <victoraweb@gmail.com>
2019-01-12 16:56:01 -0200

    [FEAT] Add custom properties

--- src/main/kotlin/springrecipes2lab/springrecipes2lab/SpringRecipes2LabApplication.kt
+++ src/main/kotlin/springrecipes2lab/springrecipes2lab/SpringRecipes2LabApplication.kt
@@ -2,7 +2,9 @@ package springrecipes2lab.springrecipes2lab
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 
+@PropertySource("classpath:custom.properties")
 @SpringBootApplication
 class SpringRecipes2LabApplication
 
